0d976c139e87329fb7b2cc75b934671d53c81815
pamelams <pamedeiross17@gmail.com>
2020-11-20 19:53:03 -0300

    classes created

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 public class App {
+    ArrayList<Collaborator> collaborators = new ArrayList<Collaborator>();
+    ArrayList<Project> projects = new ArrayList<Project>();
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
---
+++ src/Collaborator.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Collaborator {
+    private String name;
+    private String email;
+    private ArrayList<Project> history = new ArrayList<Project>(); 
+    private ArrayList<AcademicProduction> academicProduction = new ArrayList<AcademicProduction>();
+}
